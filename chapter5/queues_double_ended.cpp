5051a113a640b22fa3a16dcf124c24563646f9b0
Kamran Qadir <qadir0108@gmail.com>
2021-10-21 21:58:26 +0500

    Update queues_double_ended.cpp

--- chapter5/queues_double_ended.cpp
+++ chapter5/queues_double_ended.cpp
@@ -1,8 +1,8 @@
 /*
 	Double Ended Queues Program
 	Data Structures using C++ - Examples
-	Version:1.0
-	Date:	2020-11-09
+	Version:1.0 	Date:	2020-11-09
+	Version:1.1 	Date:	2021-10-21
 	Author:	Kamran Qadir (kamran.qadir@bzu.edu.pk)
 */
 #include<iostream>
@@ -11,20 +11,22 @@ using namespace std;
 class StudentQueue
 {
 	private:
-		int front, rear;
+		int front, back;
 		int q[10];
 	
 	public:
 		StudentQueue()
 		{
 			front = -1;
-			rear = -1; 
+			back = -1; 
 		}
 		
+		void insertBack(int n);
 		void insertFront(int n);
-		void insertRear(int n);
+		
 		void deleteFront();
-		void deleteRear();
+		void deleteBack();
+		
 		void print();
 		
 };
@@ -39,10 +41,10 @@ int main()
 	while(option != 5)
 	{
 		cout << "\n *** Double-Ended Queue Menu ***\n";
-		cout << "1 - Insert from Front\n";
-		cout << "2 - Insert from Rear\n";
-		cout << "3 - Delete from Front\n";
-		cout << "4 - Delete from Rear\n";
+		cout << "1 - Insert from BACK\n";
+		cout << "2 - Insert from FRONT\n";
+		cout << "3 - Delete from FRONT\n";
+		cout << "4 - Delete from BACK\n";
 		cout << "5 - Exit\n";
 		cout << "Enter your choice : ";
 		cin >> option;
@@ -50,25 +52,25 @@ int main()
 		switch(option)
 		{
 			case 1:
-				cout << "1 - Insert from Front\n";
+				cout << "1 - Insert from BACK\n";
+				cout << "Enter your value: ";
+				cin >> value;
+				
+				obj.insertBack(value);
+				
+				cout << "\n******* Double-Ended Queue after INSERT from BACK ******* \n";
+				obj.print();
+				
+				break;
+			
+			case 2:
+				cout << "2 - Insert from FRONT\n";
 				cout << "Enter your value: ";
 				cin >> value;
 				
 				obj.insertFront(value);
 				
 				cout << "\n******* Double-Ended Queue after INSERT from FRONT ******* \n";
-				obj.print();
-				
-				break;
-			
-			case 2:
-				cout << "2 - Insert from Rear\n";
-				cout << "Enter your value: ";
-				cin >> value;
-				
-				obj.insertRear(value);
-				
-				cout << "\n******* Double-Ended Queue after INSERT from REAR ******* \n";
 				obj.print();
 				
 				break;
@@ -82,9 +84,9 @@ int main()
 				break;
 				
 			case 4:
-				obj.deleteRear();
+				obj.deleteBack();
 				
-				cout << "\n******* Double-Ended Queue after DELETE from REAR ******* \n";
+				cout << "\n******* Double-Ended Queue after DELETE from BACK ******* \n";
 				obj.print();
 				
 				break;
@@ -97,15 +99,15 @@ int main()
 	// Member function to insert item from front side
 	void StudentQueue::insertFront(int n)
 	{
-		if(front == 0 && rear == 9)
+		if(front == 0 && back == 9)
 		{
 			cout << "Double-Ended Queue is Full... Please Wait outside...";
 			return;
 		}
 		
-		if(front == -1 && rear == -1)
+		if(front == -1 && back == -1)
 		{
-			rear = front = 0;
+			back = front = 0;
 			q[front] = n;
 		} 
 		else if(front > 0)
@@ -120,28 +122,28 @@ int main()
 		}
 	}
 	
-	// Member function to insert item from rear side
-	void StudentQueue::insertRear(int n)
+	// Member function to insert item from back side
+	void StudentQueue::insertBack(int n)
 	{
-		if(front == 0 && rear == 9)
+		if(front == 0 && back == 9)
 		{
 			cout << "Double-Ended Queue is Full... Please Wait outside...";
 			return;
 		}
 		
-		if(front == -1 && rear == -1)
+		if(front == -1 && back == -1)
 		{
-			rear = front = 0;
-			q[front] = n;
+			back = front = 0;
+			q[back] = n;
 		} 
-		else if(rear < 9)
+		else if(back < 9)
 		{
-			rear = rear + 1;
-			q[rear] = n;
+			back = back + 1;
+			q[back] = n;
 		}
 		else
 		{
-			cout << "Double-Ended Queue => No Space from REAR Side";
+			cout << "Double-Ended Queue => No Space from BACK Side";
 			return;
 		}
 	}
@@ -150,15 +152,15 @@ int main()
 	void StudentQueue::deleteFront()
 	{
 		int n;
-		if(front == -1 && rear == -1) 
+		if(front == -1 && back == -1) 
 		{
 			cout << "Double-Ended Queue is Empty";
 			return;
 		}
 		
-		if(front == rear)
+		if(front == back)
 		{
-			front = rear = -1;
+			front = back = -1;
 		} 
 		else if(front == 9)
 			front = -1;
@@ -166,22 +168,22 @@ int main()
 			front = front + 1;
 	}
 	
-	// Member function to delete item from rear side
-	void StudentQueue::deleteRear()
+	// Member function to delete item from back side
+	void StudentQueue::deleteBack()
 	{
 		int n;
-		if(front == -1 && rear == -1) 
+		if(front == -1 && back == -1) 
 		{
 			cout << "Double-Ended Queue is Empty";
 			return;
 		}
 		
-		if(front == rear)
+		if(front == back)
 		{
-			front = rear = -1;
+			front = back = -1;
 		} 
 		else
-			rear = rear - 1;
+			back = back - 1;
 	}
 	
 	// Member function to PRINT data from De-Queue
@@ -193,8 +195,8 @@ int main()
 			return;
 		}
 		
-		for(int i = front; i <= rear; i++)
-		 {
-		 	cout << q[i] << "\t"; 
-		 }
+		for(int i = front; i <= back; i++)
+		{
+			cout << q[i] << "\t"; 
+		}
 	}
